2bb6e47cb1fdeb6bbba9fc9052f844e3781cd50d
kiwijang <kiwijang5473@gmail.com>
2022-02-28 17:13:55 +0800

    format js.add {once:true}

--- js/script_ad.js
+++ js/script_ad.js
@@ -3,155 +3,204 @@ let click = "prev";
 let timer = false;
 let firstTime = false;
 
-timer = setInterval(removeHide,1500);
+timer = setInterval(removeHide, 1500);
 
-function removeHide(){
-  if(currentPage<5 && currentPage>1){
-    document.querySelector(`.mainpic>a:nth-child(${currentPage})`).classList.remove('hide');
-    document.querySelector(`.mainpic>a:nth-child(${currentPage-1})`).classList.add('hide');
-    document.querySelector(`.minipic>img:nth-child(${currentPage})`).classList.add('a');
-    document.querySelector(`.minipic>img:nth-child(${currentPage-1})`).classList.remove('a');
-    currentPage +=1;
-  }
-  else if(currentPage==5){
-    document.querySelector(`.mainpic>a:nth-child(${currentPage})`).classList.remove('hide');
-    document.querySelector(`.mainpic>a:nth-child(${currentPage-1})`).classList.add('hide');
-    document.querySelector(`.minipic>img:nth-child(${currentPage})`).classList.add('a');
-    document.querySelector(`.minipic>img:nth-child(${currentPage-1})`).classList.remove('a');
-    currentPage = 1
-  }
-  else if(currentPage==1){
-    document.querySelector(`.mainpic>a:nth-child(${5})`).classList.add('hide');
-    document.querySelector(`.mainpic>a:nth-child(${currentPage})`).classList.remove('hide');
-    document.querySelector(`.minipic>img:nth-child(${currentPage})`).classList.add('a');
-    document.querySelector(`.minipic>img:nth-child(${5})`).classList.remove('a');
-    currentPage +=1;
+function removeHide() {
+  if (currentPage < 5 && currentPage > 1) {
+    document
+      .querySelector(`.mainpic>a:nth-child(${currentPage})`)
+      .classList.remove("hide");
+    document
+      .querySelector(`.mainpic>a:nth-child(${currentPage - 1})`)
+      .classList.add("hide");
+    document
+      .querySelector(`.minipic>img:nth-child(${currentPage})`)
+      .classList.add("a");
+    document
+      .querySelector(`.minipic>img:nth-child(${currentPage - 1})`)
+      .classList.remove("a");
+    currentPage += 1;
+  } else if (currentPage == 5) {
+    document
+      .querySelector(`.mainpic>a:nth-child(${currentPage})`)
+      .classList.remove("hide");
+    document
+      .querySelector(`.mainpic>a:nth-child(${currentPage - 1})`)
+      .classList.add("hide");
+    document
+      .querySelector(`.minipic>img:nth-child(${currentPage})`)
+      .classList.add("a");
+    document
+      .querySelector(`.minipic>img:nth-child(${currentPage - 1})`)
+      .classList.remove("a");
+    currentPage = 1;
+  } else if (currentPage == 1) {
+    document.querySelector(`.mainpic>a:nth-child(${5})`).classList.add("hide");
+    document
+      .querySelector(`.mainpic>a:nth-child(${currentPage})`)
+      .classList.remove("hide");
+    document
+      .querySelector(`.minipic>img:nth-child(${currentPage})`)
+      .classList.add("a");
+    document
+      .querySelector(`.minipic>img:nth-child(${5})`)
+      .classList.remove("a");
+    currentPage += 1;
   }
 }
 
-function reverse_removeHide(){
-  if(currentPage<5 && currentPage>1){
-    document.querySelector(`.mainpic>a:nth-child(${currentPage-1})`).classList.remove('hide');
-    document.querySelector(`.mainpic>a:nth-child(${currentPage})`).classList.add('hide');
-    document.querySelector(`.minipic>img:nth-child(${currentPage-1})`).classList.add('a');
-    document.querySelector(`.minipic>img:nth-child(${currentPage})`).classList.remove('a');
-    currentPage -=1;
-  }
-  else if(currentPage==5){
-    document.querySelector(`.mainpic>a:nth-child(${currentPage-1})`).classList.remove('hide');
-    document.querySelector(`.mainpic>a:nth-child(${currentPage})`).classList.add('hide');
-    document.querySelector(`.minipic>img:nth-child(${currentPage-1})`).classList.add('a');
-    document.querySelector(`.minipic>img:nth-child(${currentPage})`).classList.remove('a');
-    currentPage -=1
-  }
-  else if(currentPage==1){
-    document.querySelector(`.mainpic>a:nth-child(${currentPage})`).classList.add('hide');
-    document.querySelector(`.mainpic>a:nth-child(${5})`).classList.remove('hide');
-    document.querySelector(`.minipic>img:nth-child(${5})`).classList.add('a');
-    document.querySelector(`.minipic>img:nth-child(${currentPage})`).classList.remove('a');
-    currentPage =5;
+function reverse_removeHide() {
+  if (currentPage < 5 && currentPage > 1) {
+    document
+      .querySelector(`.mainpic>a:nth-child(${currentPage - 1})`)
+      .classList.remove("hide");
+    document
+      .querySelector(`.mainpic>a:nth-child(${currentPage})`)
+      .classList.add("hide");
+    document
+      .querySelector(`.minipic>img:nth-child(${currentPage - 1})`)
+      .classList.add("a");
+    document
+      .querySelector(`.minipic>img:nth-child(${currentPage})`)
+      .classList.remove("a");
+    currentPage -= 1;
+  } else if (currentPage == 5) {
+    document
+      .querySelector(`.mainpic>a:nth-child(${currentPage - 1})`)
+      .classList.remove("hide");
+    document
+      .querySelector(`.mainpic>a:nth-child(${currentPage})`)
+      .classList.add("hide");
+    document
+      .querySelector(`.minipic>img:nth-child(${currentPage - 1})`)
+      .classList.add("a");
+    document
+      .querySelector(`.minipic>img:nth-child(${currentPage})`)
+      .classList.remove("a");
+    currentPage -= 1;
+  } else if (currentPage == 1) {
+    document
+      .querySelector(`.mainpic>a:nth-child(${currentPage})`)
+      .classList.add("hide");
+    document
+      .querySelector(`.mainpic>a:nth-child(${5})`)
+      .classList.remove("hide");
+    document.querySelector(`.minipic>img:nth-child(${5})`).classList.add("a");
+    document
+      .querySelector(`.minipic>img:nth-child(${currentPage})`)
+      .classList.remove("a");
+    currentPage = 5;
   }
 }
 //=====================================================================
-document.getElementById("next").addEventListener("click", skipNext);
-document.getElementById("prev").addEventListener("click", skipPrev);
-document.getElementById("play").addEventListener("click", play);
-document.getElementById("pause").addEventListener("click", pause);
+document
+  .getElementById("next")
+  .addEventListener("click", skipNext, { once: true });
+document
+  .getElementById("prev")
+  .addEventListener("click", skipPrev, { once: true });
+document.getElementById("play").addEventListener("click", play, { once: true });
+document
+  .getElementById("pause")
+  .addEventListener("click", pause, { once: true });
 
-function skipNext(){
+function skipNext() {
   console.log(currentPage);
   clearInterval(timer);
   timer = false;
   resetMinipic();
   console.log(click);
-  if(click=='prev'){
-    if(currentPage==5){
+  if (click == "prev") {
+    if (currentPage == 5) {
       currentPage = 1;
-    }else{
-      if(firstTime){
-        currentPage +=1;
+    } else {
+      if (firstTime) {
+        currentPage += 1;
       }
       firstTime = true;
     }
   }
   removeHide();
-  click = 'next';
+  click = "next";
   //隱藏暫停
-  document.getElementById(`pause`).classList.add('hide');
-  document.getElementById(`play`).classList.remove('hide');
+  document.getElementById(`pause`).classList.add("hide");
+  document.getElementById(`play`).classList.remove("hide");
 }
-function skipPrev(){
+function skipPrev() {
   console.log(currentPage);
   clearInterval(timer);
   timer = false;
   resetMinipic();
   console.log(click);
-  if(click=='next'){
-    if(currentPage==1){
+  if (click == "next") {
+    if (currentPage == 1) {
       currentPage = 5;
-    }
-    else{
-        currentPage -=1;
+    } else {
+      currentPage -= 1;
     }
   }
   reverse_removeHide();
-  click = 'prev';
+  click = "prev";
   //隱藏暫停
-  document.getElementById(`pause`).classList.add('hide');
-  document.getElementById(`play`).classList.remove('hide');
+  document.getElementById(`pause`).classList.add("hide");
+  document.getElementById(`play`).classList.remove("hide");
 }
-function pause(){
+function pause() {
   clearInterval(timer);
   timer = false;
-  document.getElementById(`pause`).classList.add('hide');
-  document.getElementById(`play`).classList.remove('hide');
+  document.getElementById(`pause`).classList.add("hide");
+  document.getElementById(`play`).classList.remove("hide");
 }
-function play(){
-  if(timer == false){
-    timer = setInterval(removeHide,1500);
+function play() {
+  if (timer == false) {
+    timer = setInterval(removeHide, 1500);
   }
-  document.getElementById(`play`).classList.add('hide');
-  document.getElementById(`pause`).classList.remove('hide');
+  document.getElementById(`play`).classList.add("hide");
+  document.getElementById(`pause`).classList.remove("hide");
 }
-function resetMinipic(){
-  let pics = document.querySelectorAll(`.minipic>img`)
-  for(let pic of pics){
-    pic.classList.remove('a');
+function resetMinipic() {
+  let pics = document.querySelectorAll(`.minipic>img`);
+  for (let pic of pics) {
+    pic.classList.remove("a");
   }
 }
-function resetMainpic(pagenow){
-  let pics = document.querySelectorAll(`.mainpic>a`)
-  for(let pic of pics){
-    pic.classList.remove('hide');
-    pic.classList.add('hide');
+function resetMainpic(pagenow) {
+  let pics = document.querySelectorAll(`.mainpic>a`);
+  for (let pic of pics) {
+    pic.classList.remove("hide");
+    pic.classList.add("hide");
   }
-  pics[pagenow-1].classList.remove('hide');
+  pics[pagenow - 1].classList.remove("hide");
 }
 //============================================================
 let minipics = document.querySelectorAll(`.minipic>img`);
-let mainpic = document.getElementById('mainpic');
-for(let minipic of minipics){
+let mainpic = document.getElementById("mainpic");
+for (let minipic of minipics) {
   //滑過
-  minipic.addEventListener('mouseover', pauseMiniShow);
+  minipic.addEventListener("mouseover", pauseMiniShow, { once: true });
   //按下滑鼠
-  minipic.addEventListener('mousedown', (e)=>{
-    resetMainpic(e.target.getAttribute('data-pic'));
-    pauseMiniShow(e);
-    pause();
-  });
+  minipic.addEventListener(
+    "mousedown",
+    (e) => {
+      resetMainpic(e.target.getAttribute("data-pic"));
+      pauseMiniShow(e);
+      pause();
+    },
+    { once: true }
+  );
   //放開滑鼠
-  minipic.addEventListener('mouseup', pauseMiniShow);
+  minipic.addEventListener("mouseup", pauseMiniShow, { once: true });
 }
 
-function pauseMiniShow(e){
+function pauseMiniShow(e) {
   pause();
   resetMinipic();
-  let picMouseover = e.target.getAttribute('data-pic');
-  currentPage = picMouseover*1;
+  let picMouseover = e.target.getAttribute("data-pic");
+  currentPage = picMouseover * 1;
   resetMainpic(currentPage);
   removeHide();
-  timer = setInterval(removeHide,1500);
+  timer = setInterval(removeHide, 1500);
   //隱藏播放
-  document.getElementById(`play`).classList.add('hide');
-  document.getElementById(`pause`).classList.remove('hide');
+  document.getElementById(`play`).classList.add("hide");
+  document.getElementById(`pause`).classList.remove("hide");
 }
--- js/script_calendar.js
+++ js/script_calendar.js
@@ -1,110 +1,127 @@
-document.addEventListener('DOMContentLoaded',()=>{
-  let selectYear = document.querySelector('#year').value;
-  let selectMonth = document.querySelector('#month').value;
-  setDateSelect(selectYear,selectMonth);
-  setCalendar();
-  document.querySelectorAll('.select_wrap > select').forEach(function(el){
-    el.addEventListener('change',function(){
-      setCalendar();
-    })
-  })
-  document.getElementById('month').addEventListener('change',function(){
-    selectYear = document.querySelector('#year').value;
-    selectMonth = document.querySelector('#month').value;
-    let selectDate = document.querySelector('#date').value;
-    setDateSelectFromMY(selectYear,selectMonth,selectDate);
-  })
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    let selectYear = document.querySelector("#year").value;
+    let selectMonth = document.querySelector("#month").value;
+    setDateSelect(selectYear, selectMonth);
+    setCalendar();
+    document.querySelectorAll(".select_wrap > select").forEach(
+      function (el) {
+        el.addEventListener("change", function () {
+          setCalendar();
+        });
+      },
+      { once: true }
+    );
+    document.getElementById("month").addEventListener(
+      "change",
+      function () {
+        selectYear = document.querySelector("#year").value;
+        selectMonth = document.querySelector("#month").value;
+        let selectDate = document.querySelector("#date").value;
+        setDateSelectFromMY(selectYear, selectMonth, selectDate);
+      },
+      { once: true }
+    );
 
-  document.getElementById('year').addEventListener('change',function(){
-    selectYear = document.querySelector('#year').value;
-    selectMonth = document.querySelector('#month').value;
-    let selectDate = document.querySelector('#date').value;
-    setDateSelectFromMY(selectYear,selectMonth,selectDate);
-  })
-});
+    document.getElementById("year").addEventListener(
+      "change",
+      function () {
+        selectYear = document.querySelector("#year").value;
+        selectMonth = document.querySelector("#month").value;
+        let selectDate = document.querySelector("#date").value;
+        setDateSelectFromMY(selectYear, selectMonth, selectDate);
+      },
+      { once: true }
+    );
+  },
+  { once: true }
+);
 //年
-(function(){
-  let html = '';
+(function () {
+  let html = "";
   let nowYear = new Date().getFullYear();
-  for(let i = nowYear - 50; i<= nowYear+50; i++){
-    html += `<option ${i===nowYear? 'selected':''}>${i}</option>`;
+  for (let i = nowYear - 50; i <= nowYear + 50; i++) {
+    html += `<option ${i === nowYear ? "selected" : ""}>${i}</option>`;
   }
   document.querySelector("#year").innerHTML = html;
 })();
 //月
-(function(){
-  let html = '';
-  let nowMonth = new Date().getMonth()+1;
-  for(let i = 1; i <= 12; i++){
-    html += `<option ${i===nowMonth? 'selected':''}>${i}</option>`;
+(function () {
+  let html = "";
+  let nowMonth = new Date().getMonth() + 1;
+  for (let i = 1; i <= 12; i++) {
+    html += `<option ${i === nowMonth ? "selected" : ""}>${i}</option>`;
   }
   document.querySelector("#month").innerHTML = html;
 })();
 //日
-function setDateSelect(year,month){
-  let html = '';
+function setDateSelect(year, month) {
+  let html = "";
   let nowDate = new Date().getDate();
-  let selectDate_last = new Date(year,month,0).getDate();
+  let selectDate_last = new Date(year, month, 0).getDate();
   console.log(selectDate_last);
   console.log(nowDate);
-  for(let i = 1; i <= selectDate_last; i++){
-    html += `<option ${i===nowDate? 'selected':''}>${i}</option>`;
+  for (let i = 1; i <= selectDate_last; i++) {
+    html += `<option ${i === nowDate ? "selected" : ""}>${i}</option>`;
   }
   document.querySelector("#date").innerHTML = html;
 }
 
 //日根據年月更新
-function setDateSelectFromMY(year,month,selectdate){
-  let html = '';
-  let selectDate_last = new Date(year,month,0).getDate();
-  for(let i = 1; i <= selectDate_last; i++){
-    html += `<option ${i==selectdate? 'selected':''}>${i}</option>`;
+function setDateSelectFromMY(year, month, selectdate) {
+  let html = "";
+  let selectDate_last = new Date(year, month, 0).getDate();
+  for (let i = 1; i <= selectDate_last; i++) {
+    html += `<option ${i == selectdate ? "selected" : ""}>${i}</option>`;
   }
   document.querySelector("#date").innerHTML = html;
 }
 
-function setCalendar(){
-  let selectYear = document.querySelector('#year').value;
-  let selectMonth = document.querySelector('#month').value;
-  let selectDate = document.querySelector('#date').value;
+function setCalendar() {
+  let selectYear = document.querySelector("#year").value;
+  let selectMonth = document.querySelector("#month").value;
+  let selectDate = document.querySelector("#date").value;
   buildCalendar(selectYear, selectMonth, selectDate);
-  document.getElementById('year_h5').innerText = selectYear+ "年";
-  document.getElementById('month_h5').innerText = selectMonth+ "月";
-  document.getElementById('date_h5').innerText = selectDate+ "日";
+  document.getElementById("year_h5").innerText = selectYear + "年";
+  document.getElementById("month_h5").innerText = selectMonth + "月";
+  document.getElementById("date_h5").innerText = selectDate + "日";
   console.log(selectDate);
 }
 
-function buildCalendar(year,month,date){
-  
-  let html = '';
-  month = month -1;
+function buildCalendar(year, month, date) {
+  let html = "";
+  month = month - 1;
   let nowdate = new Date().getDate();
   let nowMonth = new Date().getMonth();
   let nowYear = new Date().getFullYear();
   let first = new Date(year, month, 1).getDay();
-  let last = new Date(year, month+1, 0).getDate();
+  let last = new Date(year, month + 1, 0).getDate();
   let index = 1;
-  for(let rows = 0; rows < 6; rows++){
-    if(index > last) break;
-    html += '<tr>';
-    for(let cells = 0; cells < 7; cells++){
-      if(index > last) break;
-      if(cells+(rows*7)>=first && index === nowdate && month === nowMonth && year == nowYear){
+  for (let rows = 0; rows < 6; rows++) {
+    if (index > last) break;
+    html += "<tr>";
+    for (let cells = 0; cells < 7; cells++) {
+      if (index > last) break;
+      if (
+        cells + rows * 7 >= first &&
+        index === nowdate &&
+        month === nowMonth &&
+        year == nowYear
+      ) {
         html += `<td class='nowDate'>`;
-      }else if(cells+(rows*7)>=first && index == date){
+      } else if (cells + rows * 7 >= first && index == date) {
         html += `<td class='selectDate'>`;
-      }else{
+      } else {
         html += `<td>`;
       }
-      if(cells + (rows * 7) >= first){
+      if (cells + rows * 7 >= first) {
         html += index;
         index++;
       }
-      html += '</td>';
+      html += "</td>";
     }
-    html += '</tr>';
+    html += "</tr>";
   }
-  document.querySelector('tbody').innerHTML = html;
+  document.querySelector("tbody").innerHTML = html;
 }
-
-
